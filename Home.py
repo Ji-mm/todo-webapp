174dd34893b4e2b270732ca4ad8ce93d4019254b
Atudo <jimpirate49@gmail.com>
2023-01-29 17:26:55 +0300

    minor changes

--- Home.py
+++ Home.py
@@ -10,9 +10,14 @@ def add_todo():
     functions.write_todos(todos)
 
 
+sl.set_page_config(layout="wide")
 sl.title("Task Manager")
 sl.subheader("This is My-Todo App.")
-sl.write("This app is designed to help you increase your productivity.")
+sl.write("<b>This app is designed to help you increase your productivity.</b>", unsafe_allow_html=True)
+
+sl.text_input(label="Enter a new task: ", placeholder="Add a new task...",
+              on_change=add_todo, key="new_todos")
+sl.write("Check to complete a task")
 sl.write("Your pending tasks:")
 for index, todo in enumerate(todos):
     checkbox = sl.checkbox(todo.title(), key=todo)
@@ -22,7 +27,4 @@ for index, todo in enumerate(todos):
         del sl.session_state[todo]
         sl.experimental_rerun()
 
-sl.write("Tick to complete a task")
-sl.text_input(label="Enter a new task: ", placeholder="Add a new task...",
-              on_change=add_todo, key="new_todos")
 
